183e0d04c15281ee9119550ded37f374994d00ca
Anthony Tselekis <a.tselekis@gmail.com>
2025-04-25 02:16:58 +0000

    fix: update environment variable in cloud function, remove environment variable in cloud run

--- .next/static/chunks/src_app_page_tsx_cdcca6a9._.js
+++ .next/static/chunks/src_app_page_tsx_cdcca6a9._.js
@@ -1,11 +1,12 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "static/chunks/src_app_layout_tsx_f0e4c1a2._.js",
+    "static/chunks/src_app_page_tsx_cdcca6a9._.js",
     {},
 ]);
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "static/chunks/src_app_layout_tsx_f0e4c1a2._.js",
+  "path": "static/chunks/src_app_page_tsx_cdcca6a9._.js",
   "chunks": [
-    "static/chunks/[root of the server]__b07dbc43._.css"
+    "static/chunks/node_modules_1b30cb0a._.js",
+    "static/chunks/src_269deee4._.js"
   ],
   "source": "dynamic"
 });
